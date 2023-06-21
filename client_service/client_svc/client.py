f477777250c85063443953e5b81be4d106243f8f
anthonyreynaldi <77012214+anthonyreynaldi@users.noreply.github.com>
2023-06-21 15:09:18 +0700

    client flask done

--- client_service/client_svc/client.py
+++ client_service/client_svc/client.py
@@ -3,12 +3,16 @@ import mysql.connector, json, pika, logging
 from datetime import datetime
 from producer import *
 
-sql_host = 'client_service-client_sql-1'    #nama container sql
+#ganti entitynya aja sama column pas insert
+entity = 'client'
+
+sql_host = f'{entity}_service-{entity}_sql-1'    #nama container sql
 sql_host = 'localhost'
 sql_user = 'root'
 sql_pass = 'root'
 sql_pass = ''
-sql_db = 'soa_client'
+sql_db = f'soa_{entity}'
+
 
 db = mysql.connector.connect(host=sql_host, user=sql_user, password=sql_pass, database=sql_db)
 dbc = db.cursor(dictionary=True)
@@ -39,8 +43,22 @@ def db2str(data, column_names):
     print(result)
     return result
 
-@app.route('/client', methods = ['POST', 'GET'])
-def client():
+def returnResponse(dictResponse, status_code):
+    # ------------------------------------------------------
+    # Kirimkan JSON yang sudah dibuat ke client
+    # ------------------------------------------------------
+    resp = HTTPResponse()
+    jsondoc = json.dumps(dictResponse)
+    print(dictResponse)
+    print(jsondoc)
+    resp.response = jsondoc
+    resp.headers['Content-Type'] = 'application/json'
+    resp.status = status_code
+    return resp
+
+@app.route(f'/{entity}', methods = ['GET'])
+@app.route(f'/{entity}/<path:id>', methods = ['GET'])
+def client(id = None):
     response = {}   
 
     # ------------------------------------------------------
@@ -49,7 +67,11 @@ def client():
     if HTTPRequest.method == 'GET':
 
         # ambil data clients
-        sql = "SELECT * FROM clients"
+        sql = f"SELECT * FROM {entity}s"
+
+        if(id):
+            sql += f" WHERE {entity}_id = {id}"
+
         dbc.execute(sql)
         clients = dbc.fetchall()
 
@@ -64,144 +86,99 @@ def client():
             status_code = 404  # No resources found
             response['status_code'] = status_code
 
+    return returnResponse(response, status_code)
 
-    # ------------------------------------------------------
-    # HTTP method = POST
-    # ------------------------------------------------------
-    elif HTTPRequest.method == 'POST':
-        print(HTTPRequest.data)
-        x = HTTPRequest.form.get('name')
-        print("x = ", x)
-        body = json.loads(HTTPRequest.get_json())
-        print("========body========")
-        print(body)
-        kantinName = body['nama']
-        print("=======kantin name========")
-        print(kantinName)
-        gedung = body['gedung']
+
+@app.route(f'/{entity}', methods = ['POST'])
+def postClient():
+    response = {}
+
+    if HTTPRequest.method == 'POST':
+        
+        body = json.loads(HTTPRequest.get_data())
 
         try:
-            # simpan nama kantin, dan gedung ke database
-            sql = "INSERT INTO kantin_resto (client_id, name, phone_num, username, password, created_at, updated_at) VALUES (NULL, %s, %s, %s, %s, %s, %s)"
+            # simpan client
+            sql = f"INSERT INTO {entity}s ({entity}_id, name, phone_num, username, password, created_at, updated_at) VALUES (NULL, %s, %s, %s, %s, %s, %s)"
             dbc.execute(sql, [body['name'], body['phone_num'], body['username'], body['password'], datetime.now(), datetime.now()] )
             db.commit()
 
-            # dapatkan ID dari data kantin yang baru dimasukkan
-            kantinID = dbc.lastrowid
-            data_kantin = {'id':kantinID}
-            jsondoc = json.dumps(data_kantin)
+            # inserted id
+            id = dbc.lastrowid
 
+            # ambil semua data
+            sql = f"SELECT * FROM {entity}s WHERE {entity}_id = {id}"
 
-            # Publish event "new kantin" yang berisi data kantin yg baru.
-            # Data json yang dikirim sebagai message ke RabbitMQ adalah json asli yang
-            # diterima oleh route /kantin [POST] di atas dengan tambahan 2 key baru,
-            # yaitu 'event' dan kantinID.
-            # data['event']  = 'new_kantin'
-            # data['kantin_id'] = kantinID
-            # message = json.dumps(data)
-            # publish_message(message, 'client.new')
+            dbc.execute(sql)
+            json_data = db2str(dbc.fetchall(), getColumnName(dbc))[0]
 
+            # Publish event {entiy}.new
+            # Data json yang dikirim adalah semua ada yang baru diinsert
+            message = json.dumps(json_data)
+            publish_message(message, f'{entity}.new')
 
             status_code = 201
+            response['status_code'] = status_code
+            response['message'] = "Data berhasil ditambahkan"
 
         # bila ada kesalahan saat insert data\
         except mysql.connector.Error as err:
             status_code = 409
+            response['status_code'] = status_code
+            response['message'] = err.msg
 
+    return returnResponse(response, status_code)
 
-    # ------------------------------------------------------
-    # Kirimkan JSON yang sudah dibuat ke client
-    # ------------------------------------------------------
-    resp = HTTPResponse()
-    jsondoc = json.dumps(response)
-    print(response)
-    print(jsondoc)
-    resp.response = jsondoc
-    resp.headers['Content-Type'] = 'application/json'
-    resp.status = status_code
-    return resp
-
-
-
-
-
-@app.route('/client/<path:id>', methods = ['GET', 'PUT'])
-def client2(id):
-    jsondoc = ''
-
-
-    # ------------------------------------------------------
-    # HTTP method = GET
-    # ------------------------------------------------------
-    if HTTPRequest.method == 'GET':
-        response = {}   
-        if id.isnumeric():
-            sql = "SELECT * FROM clients where client_id = %s"
-            dbc.execute(sql,[id])
-            clients = dbc.fetchone()
-            print(clients)
-
-            if clients != None:
-            
-                status_code = 200  # The request has succeeded
-
-                response['data'] = db2str(clients, getColumnName(dbc))
-                response['status_code'] = status_code
-
-
-            else: 
-                status_code = 404  # No resources found
-                response['status_code'] = status_code
-
-            # ambil data kantin
-        #     sql = "SELECT * FROM kantin_resto WHERE id = %s"
-        #     dbc.execute(sql, [id])
-        #     data_kantin = dbc.fetchone()
-        #     # kalau data kantin ada, juga ambil menu dari kantin tsb.
-        #     if data_kantin != None:
-        #         sql = "SELECT * FROM kantin_menu WHERE idresto = %s"
-        #         dbc.execute(sql, [id])
-        #         data_menu = dbc.fetchall()
-        #         data_kantin['produk'] = data_menu
-        #         jsondoc = json.dumps(data_kantin)
-
-        #         status_code = 200  # The request has succeeded
-        #     else: 
-        #         status_code = 404  # No resources found
-        # else: status_code = 400  # Bad Request
-
+@app.route(f'/{entity}/<path:id>', methods = ['PUT'])
+def putClient(id):
+    response = {}
     # ------------------------------------------------------
     # HTTP method = PUT
     # ------------------------------------------------------
-    elif HTTPRequest.method == 'PUT':
-        data = json.loads(HTTPRequest.data)
-        id = data['id']
-        nama = data['nama']
-        gedung = data['gedung']
-
-        messagelog = 'PUT id: ' + str(id) + ' | nama: ' + nama + ' | gedung: ' + gedung 
-        logging.warning("Received: %r" % messagelog)
+    if HTTPRequest.method == 'PUT':
+        body = json.loads(HTTPRequest.get_data())
 
         try:
-            # ubah nama kantin dan gedung di database
-            sql = "UPDATE kantin_resto set nama=%s, gedung=%s where id=%s"
-            dbc.execute(sql, [nama,gedung,id] )
+            # update client
+            param = [datetime.now()]
+            
+            #statement query
+            sql = f"UPDATE {entity}s SET updated_at=%s, "
+
+            #concate column name based on key data
+            for column, value in body.items():
+                sql += column + "=%s, "
+                param.append(str(value))
+
+            #delete last , and concate where tablenamewhithouts_id
+            sql = sql[:-2] + f" WHERE {entity}_id=%s"
+            param.append(id)
+
+            dbc.execute(sql, param)
             db.commit()
+                                        
+            # ambil semua data
+            sql = f"SELECT * FROM {entity}s WHERE {entity}_id = {id}"
 
-            # teruskan json yang berisi perubahan data kantin yang diterima dari Web UI
-            # ke RabbitMQ disertai dengan tambahan route = 'kantin.tenant.changed'
-            data_baru = {}
-            data_baru['event']  = "updated_tenant"
-            data_baru['id']     = id
-            data_baru['nama']   = nama
-            data_baru['gedung'] = gedung
-            jsondoc = json.dumps(data_baru)
-            publish_message(jsondoc,'kantin.tenant.changed')
+            dbc.execute(sql)
+            json_data = db2str(dbc.fetchall(), getColumnName(dbc))[0]
 
-            status_code = 200
-        # bila ada kesalahan saat ubah data, buat XML dengan pesan error
+            # Publish event {entiy}.update
+            # Data json yang dikirim adalah semua ada yang baru diinsert
+            message = json.dumps(json_data)
+            publish_message(message, f'{entity}.update')
+
+            status_code = 201
+            response['status_code'] = status_code
+            response['message'] = "Data berhasil diupdate"
+
+        # bila ada kesalahan saat insert data\
         except mysql.connector.Error as err:
             status_code = 409
+            response['status_code'] = status_code
+            response['message'] = err.msg
+
+    return returnResponse(response, status_code)
 
 
 
