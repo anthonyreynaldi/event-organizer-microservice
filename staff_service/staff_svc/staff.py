e158d84b25f5f5e7a77125912b121ac27d007ddc
anthonyreynaldi <77012214+anthonyreynaldi@users.noreply.github.com>
2023-06-21 16:20:21 +0700

    staff service

--- staff_service/staff_svc/staff.py
+++ staff_service/staff_svc/staff.py
@@ -3,6 +3,7 @@ import mysql.connector, json, pika, logging
 from datetime import datetime
 from producer import *
 
+#ganti entitynya aja sama column pas insert
 entity = 'staff'
 
 sql_host = f'{entity}_service-{entity}_sql-1'    #nama container sql
@@ -11,6 +12,7 @@ sql_user = 'root'
 sql_pass = 'root'
 sql_pass = ''
 sql_db = f'soa_{entity}'
+
 
 db = mysql.connector.connect(host=sql_host, user=sql_user, password=sql_pass, database=sql_db)
 dbc = db.cursor(dictionary=True)
@@ -30,16 +32,16 @@ def getColumnName(cursor):
     column_names = [column[0] for column in cursor.description]
     return column_names
 
-def db2str(data, column_names):
+def db2str(data):
     result = []
 
     for row in data:
-        print(row)
-        # combine value with its column name
-        string_row = dict(zip(column_names, row))
         # Convert each column value to a string
-        string_row = {key: str(value) for key, value in string_row.items()}
+        string_row = {key: str(value) for key, value in row.items()}
         result.append(string_row)
+
+    print(result)
+    return result
 
 def returnResponse(dictResponse, status_code):
     # ------------------------------------------------------
@@ -54,9 +56,57 @@ def returnResponse(dictResponse, status_code):
     resp.status = status_code
     return resp
 
-@app.route('/staff', methods = ['POST'])
-def postStaff():
-    response = {} 
+def isExist(id) :
+    sql = f"SELECT * from {entity}s where {entity}_id = {id}"
+    dbc.execute(sql)
+    staffs = dbc.fetchall()
+    return len(staffs) != 0
+
+@app.route(f'/{entity}', methods = ['GET'])
+@app.route(f'/{entity}/<path:id>', methods = ['GET'])
+def client(id = None):
+    response = {}   
+
+    # ------------------------------------------------------
+    # HTTP method = GET
+    # ------------------------------------------------------
+    if HTTPRequest.method == 'GET':
+
+        # ambil data clients
+        sql = f"SELECT * FROM {entity}s"
+
+        if(id):
+            
+            if(not isExist(id)) :
+                status_code = 404  # No resources found
+                response['status_code'] = status_code
+                response['message'] = "Data not found"
+
+                return returnResponse(response, status_code)
+            
+            sql += f" WHERE {entity}_id = {id}"
+
+        dbc.execute(sql)
+        clients = dbc.fetchall()
+
+        if clients != None:
+        
+            status_code = 200  # The request has succeeded
+
+            response['data'] = db2str(clients)
+            response['status_code'] = status_code
+
+        else: 
+            status_code = 404  # No resources found
+            response['status_code'] = status_code
+
+    return returnResponse(response, status_code)
+
+
+@app.route(f'/{entity}', methods = ['POST'])
+def postClient():
+    response = {}
+
     if HTTPRequest.method == 'POST':
         
         body = json.loads(HTTPRequest.get_data())
@@ -74,12 +124,12 @@ def postStaff():
             sql = f"SELECT * FROM {entity}s WHERE {entity}_id = {id}"
 
             dbc.execute(sql)
-            json_data = db2str(dbc.fetchall(), getColumnName(dbc))[0]
+            json_data = db2str(dbc.fetchall())[0]
 
             # Publish event {entiy}.new
             # Data json yang dikirim adalah semua ada yang baru diinsert
             message = json.dumps(json_data)
-            publish_message(message, 'staff.new')
+            publish_message(message, f'{entity}.new')
 
             status_code = 201
             response['status_code'] = status_code
@@ -92,68 +142,22 @@ def postStaff():
             response['message'] = err.msg
 
     return returnResponse(response, status_code)
-        
 
-@app.route('/staff', methods = ['GET'])
-def getStaff():
-    response = {}   
-
+@app.route(f'/{entity}/<path:id>', methods = ['PUT'])
+def putClient(id):
+    response = {}
     # ------------------------------------------------------
-    # HTTP method = GET
+    # HTTP method = PUT
     # ------------------------------------------------------
-    if HTTPRequest.method == 'GET':
+    if HTTPRequest.method == 'PUT':
 
-        # ambil data clients
-        sql = "SELECT * FROM staffs"
-        dbc.execute(sql)
-        clients = dbc.fetchall()
-        print(clients)
-
-        if clients != None:
-        
-            status_code = 200  # The request has succeeded
-
-            response['data'] = db2str(clients, getColumnName(dbc))
-            response['status_code'] = status_code
-
-
-        else: 
+        if(not isExist(id)) :
             status_code = 404  # No resources found
             response['status_code'] = status_code
+            response['message'] = "Data not found"
 
-    return returnResponse(response, status_code)
+            return returnResponse(response, status_code)
 
-
-@app.route('/staff/<path:id>', methods = ['GET'])
-def getParticularStaff(id):
-    jsondoc = ''
-
-    if HTTPRequest.method == 'GET':
-        response = {}   
-        if id.isnumeric():
-            sql = "SELECT * FROM staffs where staff_id = %s"
-            dbc.execute(sql, [id])
-            clients = dbc.fetchone()
-            print(clients)
-
-            if clients != None:
-            
-                status_code = 200  # The request has succeeded
-
-                response['data'] = db2str(clients, getColumnName(dbc))
-                response['status_code'] = status_code
-
-
-            else: 
-                status_code = 404  # No resources found
-                response['status_code'] = status_code
-
-        return returnResponse(response, status_code)
-
-@app.route('/staff/<path:id>', methods = ['PUT'])
-def updateParticularStaff(id) :
-    response = {}
-    if HTTPRequest.method == 'PUT':
         body = json.loads(HTTPRequest.get_data())
 
         try:
@@ -179,7 +183,66 @@ def updateParticularStaff(id) :
             sql = f"SELECT * FROM {entity}s WHERE {entity}_id = {id}"
 
             dbc.execute(sql)
-            json_data = db2str(dbc.fetchall(), getColumnName(dbc))[0]
+            json_data = db2str(dbc.fetchall())[0]
+
+            # Publish event {entiy}.update
+            # Data json yang dikirim adalah semua ada yang baru diinsert
+            message = json.dumps(json_data)
+            publish_message(message, f'{entity}.update')
+
+            status_code = 201
+            response['status_code'] = status_code
+            response['message'] = "Data berhasil diupdate"
+
+        # bila ada kesalahan saat insert data\
+        except mysql.connector.Error as err:
+            status_code = 409
+            response['status_code'] = status_code
+            response['message'] = err.msg
+
+        return returnResponse(response, status_code)
+
+@app.route(f'/{entity}/<path:id>', methods = ['PUT'])
+def putClient(id):
+    response = {}
+    # ------------------------------------------------------
+    # HTTP method = PUT
+    # ------------------------------------------------------
+    if HTTPRequest.method == 'PUT':
+        
+        if(not isExist(id)) :
+            status_code = 404  # No resources found
+            response['status_code'] = status_code
+            response['message'] = "Data not found"
+
+            return returnResponse(response, status_code)
+        
+        body = json.loads(HTTPRequest.get_data())
+
+        try:
+            # update client
+            param = [datetime.now()]
+            
+            #statement query
+            sql = f"UPDATE {entity}s SET updated_at=%s, "
+
+            #concate column name based on key data
+            for column, value in body.items():
+                sql += column + "=%s, "
+                param.append(str(value))
+
+            #delete last , and concate where tablenamewhithouts_id
+            sql = sql[:-2] + f" WHERE {entity}_id=%s"
+            param.append(id)
+
+            dbc.execute(sql, param)
+            db.commit()
+                                        
+            # ambil semua data
+            sql = f"SELECT * FROM {entity}s WHERE {entity}_id = {id}"
+
+            dbc.execute(sql)
+            json_data = db2str(dbc.fetchall())[0]
 
             # Publish event {entiy}.update
             # Data json yang dikirim adalah semua ada yang baru diinsert
@@ -197,8 +260,6 @@ def updateParticularStaff(id) :
             response['message'] = err.msg
 
     return returnResponse(response, status_code)
-
-
 
 
 
